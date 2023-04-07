2d5f15e616fdda7b99ae2be4a6116ba8349ce116
Oksana-perederii <novamova.nemchenko@gmail.com>
2023-04-07 20:37:48 +0300

    Hw-07-js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const imageList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<ul class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+    />
+    </a>
+    </ul>`;
+  })
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", imageList);
 
-console.log(galleryItems);
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
